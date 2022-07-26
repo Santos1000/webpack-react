6db09386d1f1304ab81bd46115c838b55859f1b7
barbara santos <barbaransantos1@gmail.com>
2022-07-26 20:07:23 -0300

    first commit

---
+++ src/greet.js
@@ -0,0 +1,8 @@
+import { capitalize } from "./capitalize";
+
+function greet(Name) {
+  debugger
+  return `Hello ${ capitalize(Name) }`;
+}
+
+export { greet };
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { greet } from "./greet";
+import $ from 'jquery';
+
+const name ="Roma";
+
+console.log(greet(name));
